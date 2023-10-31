084251d23b04631dade8ad7f24a8ac8ec3b8f5d7
FelipeOFF <felipe-not@hotmail.com>
2023-10-31 16:26:00 -0300

    configure project

---
+++ lib/src/mzic_image_assets_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MzicImageAssetsScreen extends StatelessWidget {
+  const MzicImageAssetsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
