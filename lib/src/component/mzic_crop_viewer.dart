f4c178260cbb4fb47136850e3bd2dc671f964bdb
FelipeOFF <felipe-not@hotmail.com>
2023-10-31 17:37:56 -0300

    add some controllers to mzic image crop

--- lib/src/component/mzic_crop_viewer.dart
+++ lib/src/component/mzic_crop_viewer.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mzic_image_assets_picker/src/controller/mzic_image_assets_controller.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
 class MzicCropViewer extends StatefulWidget {
+  final MzicImageAssetsCropViewController? controller;
+  final AssetEntity? assetEntity;
+
   const MzicCropViewer({
     super.key,
+    this.controller,
+    this.assetEntity,
   });
 
   @override
@@ -10,8 +17,30 @@ class MzicCropViewer extends StatefulWidget {
 }
 
 class MzicCropViewerState extends State<MzicCropViewer> {
+  MzicImageAssetsCropViewController get controller =>
+      widget.controller ?? MzicImageAssetsCropViewControllerGeneric();
+
+  @override
+  void initState() {
+    super.initState();
+    if (controller is MzicImageAssetsCropViewControllerGeneric) {
+      controller.previewAsset = widget.assetEntity;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ValueListenableBuilder(
+      valueListenable: controller.previewAssetVN,
+      builder: (context, assetSelected, _) {
+        return Container();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
--- lib/src/controller/mzic_image_assets_controller.dart
+++ lib/src/controller/mzic_image_assets_controller.dart
@@ -1,3 +1,27 @@
-class MzicImageAssetsController {
+import 'package:flutter/material.dart';
+import 'package:wechat_assets_picker/wechat_assets_picker.dart';
 
+abstract class BaseMzicImageAssetsController {
+  void dispose() {}
+}
+
+class MzicImageAssetsController extends BaseMzicImageAssetsController
+    with MzicImageAssetsCropViewController {}
+
+class MzicImageAssetsCropViewControllerGeneric
+    extends BaseMzicImageAssetsController
+    with MzicImageAssetsCropViewController {}
+
+mixin MzicImageAssetsCropViewController on BaseMzicImageAssetsController {
+  final ValueNotifier<AssetEntity?> previewAssetVN =
+      ValueNotifier<AssetEntity?>(null);
+
+  AssetEntity? get previewAsset => previewAssetVN.value;
+  set previewAsset(AssetEntity? value) => previewAssetVN.value = value;
+
+  @override
+  void dispose() {
+    previewAssetVN.dispose();
+    super.dispose();
+  }
 }
